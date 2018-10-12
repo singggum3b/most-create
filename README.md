9c8ee5e20a034622b3990efdda178971ba962aac
singgum3b <singgum3b@gmail.com>
2018-10-12 11:13:27 +0700

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # most-create
-Create imperative stream for mostjs, useful for bridging custom events source (e.g react)
+> Create imperative stream for mostjs, useful for bridging custom events source (e.g react)
+
+[![npm](https://img.shields.io/npm/v/most-create.svg)](https://www.npmjs.com/package/most-create)
+
+## Install
+
+```bash
+npm i -S most-create
+```
+
+## Usage
+
+### create
+
+Create a mostjs stream with ability to manually emit event. Based on Proxy
+
+Type: `function`
+
+Context: `any`
+
+Target: `any`
+
+Produce: `stream`
+
+```typescript
+    private inputChangeStream = create<React.SyntheticEvent<HTMLInputElement>>("inputChangeStream");
+    ...
+    this.inputChangeStream.observe((e) => {
+    	this.model.setDisplayValue(e.currentTarget.value);
+    });
+    ...
+    public onInputChange = (e: React.SyntheticEvent<HTMLInputElement>): void => {
+    	this.inputChangeStream.emit(e);
+    };
+```
+
+
+
+## License
+
+Apache License 2.0
+
+[npm-image]: https://img.shields.io/npm/v/live-xxx.svg
+[npm-url]: https://npmjs.org/package/live-xxx
+[travis-image]: https://img.shields.io/travis/live-js/live-xxx/master.svg
+[travis-url]: https://travis-ci.org/live-js/live-xxx
+[coveralls-image]: https://img.shields.io/coveralls/live-js/live-xxx/master.svg
+[coveralls-url]: https://coveralls.io/r/live-js/live-xxx?branch=master
---
+++ create.ts
@@ -0,0 +1,78 @@
+import symbolObservable from "symbol-observable"
+import {from, Observable, Stream, Subscriber, Subscription} from "most";
+
+function observe<T extends object>(o: T, callback: (s: any, v: any) => boolean): { proxy: T, revoke: () => void } {
+	return Proxy.revocable(o, {
+		set(target: T, property: string, value: any): boolean {
+			(target as any)[property] = value;
+			return callback(property, value);
+		},
+	});
+}
+
+class ProxyObservable<T> implements Observable<T> {
+
+	public name: string;
+
+	private boxed: { value: T | null };
+	private revoke: () => void;
+
+	private subscriberList: Array<Subscriber<T>> = [];
+
+	constructor(name: string) {
+
+		this.name = name;
+
+		const { proxy, revoke } = observe({
+			value: null,
+		}, (_, value) => {
+			this.subscriberList.forEach((subscriber) => {
+				try {
+					if (this.subscriberList.some(i => i === subscriber)) {
+						subscriber.next(value);
+					}
+				} catch (e) {
+					console.error(e);
+					subscriber.error(e);
+				}
+			});
+			return true;
+		});
+
+		this.boxed = proxy;
+		this.revoke = revoke;
+	}
+
+	public [symbolObservable](): ProxyObservable<T> {
+		return this;
+	}
+
+	public subscribe(subscriber: Subscriber<T>): Subscription<T>  {
+		this.subscriberList.push(subscriber);
+
+		return {
+			unsubscribe: (): void => {
+				this.subscriberList = this.subscriberList.filter(i => i !== subscriber);
+			},
+		}
+	}
+
+	public destroy(): void {
+		this.revoke();
+	}
+
+	public emit = (e: T): void => {
+		this.boxed.value = e;
+	}
+}
+
+export interface ISelfEmitStream<T> extends Stream<T> {
+	emit: (e: T) => void
+}
+
+export function create<T extends (string | number | boolean | object)>(name: string): ISelfEmitStream<T> {
+	const value = new ProxyObservable<T>(name);
+	const stream = from<T>(value) as ISelfEmitStream<T>;
+	stream.emit = value.emit;
+	return stream;
+}
